d4901d1506543bb6a071e1fa11b61630a992f8ed
hermannlerusse <adjeakakpo@gmail.com>
2022-10-30 20:38:15 +0100

    ajout de getter et setter sur user

--- src/main/java/com/projet/housing/model/User.java
+++ src/main/java/com/projet/housing/model/User.java
@@ -93,6 +93,22 @@ public class User implements Serializable{
         return id;
     }
 
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
     public String getUsername() {
         return username;
     }
